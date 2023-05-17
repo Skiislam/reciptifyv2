0e68b53c67d6bc6d53aacd5e474e0b1f08cad215
skiislam <sisla016@ucr.edu>
2023-05-17 15:50:23 -0700

    Added new logo

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import './homepage.css';
-import iphoneImage from './Assets/Iphone.png'
+import iphoneImage from './Assets/Iphone.png';
+import AppleMusicLogo from './Assets/appleMusic.png';
+import SpotifyLogo from './Assets/spotifyLogo.png';
 
 export default function Homepage() {
 
 
     return(
+
+       
+    <Fragment>     
 
     <section className="section1">
         <div className="LanderSection1"> 
@@ -19,12 +24,28 @@ export default function Homepage() {
                 <p className="paragraph1"> Ever wonder what your most listened to songs on a reciept would look like.
                     We did not either, but here it is, sign in below using Spotify to get started
                 </p>
-                <button className="spotifyLogin"> Log In with Spotify</button>
+                <button className="spotifyLogin"> Log In with Spotify</button>            
+            </div>
+            </div>
+
+
+        </section>
+        <section className="section2">
+        <h1 className="title1"> Currently Compatiable with Popular Music Streaming Apps</h1>
+            <div className="LanderSection2"> 
+            <div className="appleMusicLogo"> 
             
+            <img style={{width: '150px'}} src={AppleMusicLogo}/>
+
+            </div>
+            <div className="spotifyLogo">
+            <img style={{width: '150px'}} src={SpotifyLogo}/>
+
             </div>
             </div>
 
         </section>
+        </Fragment>
 
 
     );
